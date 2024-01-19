77ee7f74bc4e8dc4cd4da55fd1c44e18b42af793
Betty033 <geremewbetelhem@gmail.com>
2024-01-19 14:25:39 +0200

    basic_02

---
+++ basic function/basic_02.js
@@ -0,0 +1,12 @@
+// Write the function multiply such that it returns the product (the result of multiplication) of a and b.
+let x
+let y
+function multiply (x, y){
+
+    x*y;
+    
+    return x*y;
+}
+// sample usage - do not modify
+console.log(multiply(2, 4));
+console.log(multiply(3, 2));
