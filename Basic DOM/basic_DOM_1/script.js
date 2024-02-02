90b5d593a8c967ecfb0f4a75de9cd1cb7925e7f5
Betty033 <geremewbetelhem@gmail.com>
2024-02-02 14:54:27 +0200

    exercise 1

---
+++ Basic DOM/basic_DOM_1/script.js
@@ -0,0 +1,15 @@
+/* 
+
+Write JavaScript code that selects content id by using getElementById.
+Write a function which will update the text content of the selected element to "Hello, World!" by clicking the button.
+*/
+
+
+function changetext() {
+    const text = document.getElementById('content');
+    //text.innerHTML = 'Hello World';
+    text.textContent = 'Hello World';
+    console.log(text.textContent);
+}
+
+//text.addEventListener('click', changetext);
