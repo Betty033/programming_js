45ac091796ac8d0308aaf45690f3a90d7c13f45b
Betty033 <geremewbetelhem@gmail.com>
2024-02-05 09:51:22 +0200

    done

---
+++ conditions/conditions_06.js
@@ -0,0 +1,21 @@
+/**
+Develop a function called greaterNum which:
+
+ - Accepts two arguments, both of which are numbers.
+ - Returns the larger of the two numbers.
+
+ */
+ function greatNum(a, b){
+if (a>b){
+    return a;
+
+} else {
+
+    return b;
+}
+return a> b ? a: b;s
+ }
+// Sample usage - do not modify
+console.log(greaterNum(5, 10));
+console.log(greaterNum(2, 1));
+console.log(greaterNum(4, 25));
